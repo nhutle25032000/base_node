3c1a3d75f14a343726b2f375f8ebeb9c9f48b493
Mac <mac@Macs-MacBook-Pro-2.local>
2023-10-01 14:07:26 +0700

    update connect DB

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -4,6 +4,17 @@ async function connect() {
     try {
         await mongoose.connect('mongodb://127.0.0.1:27017/test_pro');
         console.log('connect successfully !!!');
+        
+        const db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'Lỗi kết nối đến MongoDB:'));
+        db.on('disconnected', function () {
+            console.log(`disconnected::: ${this.name}`);
+        })
+
+        process.on('SIGINT', async () => {
+            await db.close();
+            process.exit(0);
+        })
     } catch (error) {
         console.log(error);
     }
